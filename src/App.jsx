1267213b9c1b6cda40adfe7327fd244b26c8659f
dilyara <di.salakhovaa@mail.ru>
2023-01-13 14:54:00 +0300

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,6 +31,7 @@ function App() {
       </p>
 
       <Header />
+      <Footer />
       <h1>Hello</h1>
       <p>Paragraph</p>
     </div>
